9dae13f33fb41743fd861feeeec6f296162aeb43
Mahtab Alam <alammahtab08@gmail.com>
2017-04-16 09:37:27 +0530

    Added 2 new tests

---
+++ src/test/java/org/cghr/Test4.java
@@ -0,0 +1,19 @@
+package org.cghr;
+
+/**
+ * Created by mahtab on 7/4/17.
+ */
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class Test3 {
+
+    @Test
+    public void test3() {
+        String str="Test3";
+
+        Assert.assertEquals(str, "Test3");
+
+    }
+
+}
